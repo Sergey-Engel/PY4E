660484ff77ac74d53ea9afa16288735d2d286d5e
Sergey Engel <sengel@mail.ru>
2020-02-08 09:09:40 +0300

    Добавлены () к функции sum -> sum()

--- Lesson-3/14-regexp-2.py
+++ Lesson-3/14-regexp-2.py
@@ -5,5 +5,4 @@ name = input("Enter file:")
 if len(name) < 1 : name = "regex_sum_42.txt"
 handle = open(name)
 
-
-print(sum[int(item) for item in re.findall('[0-9]+',handle.read())])
+print(sum([int(item) for item in re.findall('[0-9]+',handle.read())]))
