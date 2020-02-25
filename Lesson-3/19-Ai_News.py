33783c8ef4eb41398ee9ac5e21f631f44c42e846
Sergey Engel <sengel@mail.ru>
2020-02-25 13:10:53 +0300

    Добавлено сравнение старого и нового списка URL

--- Lesson-3/19-Ai_News.py
+++ Lesson-3/19-Ai_News.py
@@ -18,10 +18,58 @@ html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 tags = soup('a')
 
-count = 0
-for tag in tags :
-    count = count + 1
-    #print('TAG',tag)
-    print('URL-'+str(count), tag.get('href', None))
-    #print('CONTENT',tag.contents[0])
-    #print('ATTRs:', tag.attrs)
+fname = 'ai_news.txt'
+foldname = 'ai_news_old.txt'
+try:
+    fh = open(fname,'w')
+except:
+    print('ERROR -- File \"'+fname+'\" can not be open')
+    quit()
+try:
+    foh = open(foldname,'r')
+except:
+    print('ERROR -- File \"'+foldname+'\" can not be open')
+    quit()
+
+#print('TAG',tag)
+#print('URL-'+str(count), tag.get('href', None))
+#print('CONTENT',tag.contents[0])
+#print('ATTRs:', tag.attrs)
+
+urllist = list()
+for tag in tags : urllist.append(tag.get('href', None))
+urllist.sort()
+urlnewlist = list()
+
+prevline = ''
+for line in urllist :
+    if line == prevline : continue
+    urlnewlist.append('https://ai-news.ru/'+line+'\n')
+    prevline = line
+
+for line in urlnewlist: fh.write(line)
+
+urloldlist = list()
+for oldline in foh :
+#    oldline.rstrip()
+    urloldlist.append(oldline)
+
+print('Total NEW links:',len(urlnewlist))
+print('Total OLD links:',len(urloldlist))
+
+new = 0
+old = 0
+for i in range(0,max(len(urlnewlist),len(urloldlist))) :
+#for i in range(0,20) :
+    if urlnewlist[new] == urloldlist[old] :
+        new = new + 1
+        old = old + 1
+        continue
+    if urlnewlist[new] > urloldlist[old] :
+        print('--- ('+str(old)+'):',urloldlist[old].rstrip())
+        old = old + 1
+        continue
+    if urlnewlist[new] < urloldlist[old] :
+        print('+++ ('+str(new)+'):',urlnewlist[new].rstrip())
+        new = new + 1
+        continue
